e307c4b2d44b81b07e28535db3c468970d9daeeb
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-04-30 10:45:03 -0500

    Modified PetFinder API call to generate new token upon find pets button click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const apiKey = "koiPttYEv8zC3GNcbp6KImr8XI1IUdRJUJPnKb4E7NNFqw7dJM";
-const apiToken =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJrb2lQdHRZRXY4ekMzR05jYnA2S0ltcjhYSTFJVWRSSlVKUG5LYjRFN05ORnF3N2RKTSIsImp0aSI6ImJlM2E0MTY3YjIyNGMxY2U4N2E3NzNmNTcwYTNiNmY2Y2U4OThhNDMzZWQxNzUzNzM3MzVmNDBlZTkyOTA3NDA0YjJmYzI4YjM4ZGY0NDQxIiwiaWF0IjoxNjgyNjE1OTg1LCJuYmYiOjE2ODI2MTU5ODUsImV4cCI6MTY4MjYxOTU4NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.FFEiobG-qT9v0C8BhRpoxx4JV3KItBptSsF8scZAFO3a-YtM9TWwLNkTvnERYLpPMsNFhsVfA9IVoiRqLfpA1H6qxmgFkoG-THWfkW90_zNVIWP_s7bW3WEcOh9GOTLj2D1wtFFtLa6l6uSNdpFC7LSDCvyBDImk-zFGOpWh-rCo8lLF_LNTtF8idSj2lI40rI1dLz9dtAwAS7u6AispFvStF3MP1UAoSJM8CQmU49mJOFoKGDW1JNDx6KXUHSv9wubfw7hct782S5dCSHoYfsRXwOWL68r-zaSUaWnG7Ri9FvRH7HP8LDKUut7kRnkK5AOVEeSRGp1ou1ctmxVhDA";
+const apiSecret = "hma7lW4o0Qlcj2qjBnY0hHSTTeml2dqRTckHTc56";
+
 const petUrl = "https://api.petfinder.com/v2/animals";
 const availablePets = $("#availablePets");
 
 const pets = {
-  form() {
+  async form() {
     $("#petForm").on("submit", (e) => {
       e.preventDefault();
       const userLocation = $(".currentLocation").val();
@@ -16,43 +16,57 @@ const pets = {
     });
   },
 
-  petsCall(userLocation, petType, petSex) {
-    console.log(userLocation, petType, petSex);
-    $.ajax({
-      url: petUrl,
-      method: "GET",
-      dataType: "json",
+  async fetchToken() {
+    const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
       headers: {
-        Authorization: `Bearer ${apiToken}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
-      data: {
-        location: userLocation,
-        type: petType,
-        gender: petSex,
-        status: "adoptable",
-        age: "senior",
-        limit: 10,
-      },
-      success: (results) => {
-        const petResults = results.animals;
-        console.log(petResults);
-        for (let i = 0; i < petResults.length; ++i) {
-          const petName = petResults[i].name;
-          const petPhoto = petResults[i].photos.length
-            ? petResults[i].photos[0].small
-            : "https://via.placeholder.com/150";
-
-          console.log(petName);
-          console.log(petPhoto);
-          availablePets.append(`<p>${petName}</p>`);
-          availablePets.append(`<div><img src="${petPhoto}"></div>`);
-        }
-      },
-      error: (error) => {
-        console.log(error);
-      },
+      body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch access token. Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.access_token;
   },
+
+  async petsCall(userLocation, petType, petSex) {
+    console.log(userLocation, petType, petSex);
+
+    try {
+      const apiToken = await pets.fetchToken();
+      const response = await fetch(`${petUrl}?location=${userLocation}&type=${petType}&gender=${petSex}&status=adoptable&age=senior&limit=10`, {
+        headers: {
+          'Authorization': `Bearer ${apiToken}`
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch pets. Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const petResults = data.animals;
+      console.log(petResults);
+      availablePets.empty();
+      for (let i = 0; i < petResults.length; ++i) {
+        const petName = petResults[i].name;
+        const petPhoto = petResults[i].photos.length
+          ? petResults[i].photos[0].small
+          : "https://via.placeholder.com/150";
+
+        console.log(petName);
+        console.log(petPhoto);
+        availablePets.append(`<p>${petName}</p>`);
+        availablePets.append(`<div><img src="${petPhoto}"></div>`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
 $(document).ready(() => {
